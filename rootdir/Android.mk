99cd5c7204dd67dcf988f84030270ea6c97471e7
Akshay Menon <deadlyindian@users.noreply.github.com>
2017-08-11 09:28:57 +0000

    Fix again

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -84,10 +84,3 @@ LOCAL_SRC_FILES    := etc/init.spectrum.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.supolicy.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.supolicy.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
