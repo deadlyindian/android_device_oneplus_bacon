dfa7f7855278e06db810e677009e6bedb9ec6216
Akshay Menon <deadlyindian@users.noreply.github.com>
2017-08-17 17:41:13 +0000

    Switch to arm-eabi-
    Signed-off-by: Akshay Menon <deadlyindian@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,8 +52,8 @@ BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-7.x/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-7.x/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
