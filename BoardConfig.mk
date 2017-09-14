4645fa4932fa4ad2ab70836dce5558577b10c3af
Akshay Menon <deadlyindian@users.noreply.github.com>
2017-09-14 18:17:12 +0000

    Toolchain: Switch to UberTC-7.x
    Signed-off-by: Akshay Menon <deadlyindian@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,7 @@ BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.x/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 # ANT+
