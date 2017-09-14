d4eed3ef109a3f69e5dfd00c467be9c7d2e40922
Akshay Menon <deadlyindian@users.noreply.github.com>
2017-09-14 18:13:02 +0000

    DU-ify
    Signed-off-by: Akshay Menon <deadlyindian@users.noreply.github.com>

--- du.mk
+++ du.mk
@@ -18,10 +18,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bacon device
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Dirty stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_bacon
+DU_BUILD_TYPE := Deadly-Optimized
+PRODUCT_NAME := du_bacon
 PRODUCT_DEVICE := bacon
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := A0001
@@ -33,9 +34,6 @@ TARGET_VENDOR := oneplus
 TARGET_VENDOR_PRODUCT_NAME := bacon
 TARGET_VENDOR_DEVICE_NAME := A0001
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001 PRODUCT_NAME=bacon
-
-# Unofficial build ID
-TARGET_UNOFFICIAL_BUILD_ID := Sultan
 
 ## Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
