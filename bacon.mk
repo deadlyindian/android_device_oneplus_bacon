4ac13221d2be5fc719cbeea5f4992f9543663802
Akshay Menon <deadlyindian@users.noreply.github.com>
2018-03-30 13:44:40 +0000

    bacon: Remove deprecated audiod
    Signed-off-by: Akshay Menon <deadlyindian@users.noreply.github.com>

--- bacon.mk
+++ bacon.mk
@@ -56,7 +56,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    audiod \
+    android.hardware.audio.effect@2.0-service \
     audio.a2dp.default \
     audio_policy.msm8974 \
     audio.primary.msm8974 \
