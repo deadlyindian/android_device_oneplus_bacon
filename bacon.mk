75f2c6617b41ca81be2bda85487e2c11bac9f483
Bruno Martins <bgcngm@gmail.com>
2018-07-16 22:11:18 +0000

    bacon: Drop deprecated OpenGLRenderer props
     * In Android 8.1 and later, only the ro.zygote.disable_gl_preload
       property still applies. All other properties have been removed.
       Reference: https://source.android.com/devices/graphics/renderer
    
    Signed-off-by: Akshay Menon <deadlyindian@users.noreply.github.com>

--- bacon.mk
+++ bacon.mk
@@ -118,20 +118,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl
 
-# HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=96 \
-    ro.hwui.layer_cache_size=64 \
-    ro.hwui.r_buffer_cache_size=12 \
-    ro.hwui.path_cache_size=39 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=7 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=2048 \
-    ro.hwui.text_small_cache_height=2048 \
-    ro.hwui.text_large_cache_width=3072 \
-    ro.hwui.text_large_cache_height=2048
-
 # Data
 PRODUCT_PACKAGES += \
     librmnetctl
